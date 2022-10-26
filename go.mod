1fe631976b21ca9e46286dbaa3e1bec79608cbe5
klover <49420645@qq.com>
2022-10-26 13:57:29 +0800

    1.0.0

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/klover2/qbit-go-sdk
 
-go 1.19
+go 1.16
--- main.go
+++ main.go
@@ -3,29 +3,23 @@ package main
 import (
 	"fmt"
 
-	"github.com/klover2/qbit-go-sdk/utils"
+	qbit "github.com/klover2/qbit-go-sdk/services"
 )
 
 func main() {
-	params := map[string]interface{}{
-		"id":                  "ee74c872-8173-4b67-81b1-5746e7d5ab88",
-		"accountId":           nil,
-		"holderId":            "d2bd6ab3-3c28-4ac7-a7c4-b7eed5eee367",
-		"currency":            "USD",
-		"settlementCurrency":  nil,
-		"counterparty":        "SAILINGWOOD;;US;1800948598;;091000019",
-		"transactionAmount":   11,
-		"fee":                 0,
-		"businessType":        "Inbound",
-		"status":              "Closed",
-		"transactionTime":     "2021-11-22T07:34:10.997Z",
-		"transactionId":       "124d3804-defa-4033-9f30-1d8b0468e506",
-		"clientTransactionId": nil,
-		"createTime":          "2021-11-22T07:34:10.997Z",
-		"appendFee":           0,
+	service := qbit.NewRequestService()
+	service.SetAccessToken("c93bdb64d687a3fa9e13b9742d0fa9397e495245")
+	body := map[string]interface{}{
+		"name": "预算名",
+		"cost": 10,
 	}
-	fmt.Println(utils.Sign(utils.JoinStr(params), "25d55ad283aa400af464c76d713c07ad"))
-	// => 8287d5539c03918c9de51176162c2bf7065d5a8756b014e3293be1920c20d102
+	// res, err := service.GetRequest("http://127.0.0.1:3000/open-api/v1/budget", query)
+	res, err := service.PostRequest("http://127.0.0.1:3000/open-api/v1/budget", body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res)
 }
 
 // func main() {
